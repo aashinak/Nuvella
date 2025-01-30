f8e2b30f24da86f77bb7e7a65d7939c0585125ca
aashinak <aashin9605@gmail.com>
2025-01-30 09:23:22 +0530

    update IUserCart interface to allow productId as a string or IProduct

--- server/src/entities/user/IUserCart.ts
+++ server/src/entities/user/IUserCart.ts
@@ -2,7 +2,7 @@ import IProduct from "../product/IProduct";
 
 export default interface IUserCart {
   _id?: string;
-  productId: IProduct;
+  productId: string | IProduct;
   userId: string;
   size: string;
 }
