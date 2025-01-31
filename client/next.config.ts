6479d04414dfd72277a17f6e78d43a26dc5b73f6
aashinak <aashin9605@gmail.com>
2025-01-31 20:54:02 +0530

    initialize Next.js project with ESLint, Tailwind CSS, and basic configuration files

---
+++ client/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: ['res.cloudinary.com', 'img.freepik.com'], // Add the external domain here
+
+  },
+};
+
+export default nextConfig;
