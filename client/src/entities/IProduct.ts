5dc9ae8e493dd604dfa4fef2c95ebb9937f42ed4
aashinak <aashin9605@gmail.com>
2025-02-01 09:00:31 +0530

    add TypeScript interfaces for categories and products

---
+++ client/src/entities/IProduct.ts
@@ -0,0 +1,12 @@
+export default interface IProduct {
+    _id?: string;
+    name: string;
+    description: string;
+    images: string[];
+    price: string;
+    categoryId: string;
+    stock: number;
+    sizes?: { size: string; stock: number }[];
+    discountId?: string;
+  }
+  
