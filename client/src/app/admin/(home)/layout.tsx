71e54abef5c4fa4a9b1ee914244a5000bdcfd9f0
Aashin A K <88496253+aashinak@users.noreply.github.com>
2025-02-15 15:19:41 +0530

    Update layout.tsx

--- client/src/app/admin/(home)/layout.tsx
+++ client/src/app/admin/(home)/layout.tsx
@@ -1,12 +1,15 @@
-"use client"
+"use client";
+import React, { ReactNode } from "react";
 import AdminNavbar from "@/components/admin/homeLayout/adminNavbar";
 import AdminSidebar from "@/components/admin/homeLayout/adminSidebar";
 import LayoutContentWraper from "@/components/admin/homeLayout/layoutContentWraper";
 import LayoutWraper from "@/components/admin/homeLayout/layoutWraper";
 
-import React from "react";
+interface LayoutProps {
+  children: ReactNode;
+}
 
-function Layout({ children }) {
+function Layout({ children }: LayoutProps) {
   return (
     <LayoutWraper>
       <AdminNavbar />
